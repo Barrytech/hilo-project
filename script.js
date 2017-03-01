ef70da7cd724c8f6b99edf4e5262097595ae5bdc
Barry <barry@citypolyhs.org>
2017-03-01 20:07:09 +0000

    we change the append into the replace

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ $(document).ready(function() {
             input = Number($("input").val());
        
         if (input===7) {
-            $("#result").append("OOOOOOOOHHHH You smart, arent you!!!");
+            $("#result").html("OOOOOOOOHHHH You smart, arent you!!!");
         }
         else if (input < 7) {
-            $("#result").append("wrong guess! Too low");
+            $("#result").html("wrong guess! Too low");
         }
         else if (input > 7) {
-            $("#result").append("wrong guess! To high");
+            $("#result").html("wrong guess! To high");
         }
 
     });
